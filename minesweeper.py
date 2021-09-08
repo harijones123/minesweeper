b81a06f4f8dd61cb3ba8dd5ba912d3f41470e4d2
HariJones97 <61684178+HariJones97@users.noreply.github.com>
2021-09-09 00:32:36 +0100

    Added player, more work needed

--- minesweeper.py
+++ minesweeper.py
@@ -11,14 +11,14 @@ def user_input():
     cell = tuple([int(x),int(y)])
     return type.upper(),cell
 
-class game:
+class Game:
     def __init__(self, rows, cols, n_mines):
         self.rows = rows
         self.cols = cols
         self.n_mines = n_mines
         self.board = [[0 for i in range(0,rows)] for j in range(0,cols)]
         self.board_coords = [(x,y) for x in range(0,cols) for y in range(0,rows)]
-        self.board_revealed = [[False for i in range(0,rows)] for j in range(0,cols)]
+        self.revealed_coords = set([])
         self.gameState = "playing"
     def make_first_move(self,selection=None):
         if selection is None:
@@ -57,11 +57,11 @@ class game:
     def flag_cell(self,cell):
         x,y = cell
         self.board[x][y] = 10
-        self.board_revealed[x][y] = True
+        self.revealed_coords.add((x,y))
     def reveal_cell(self,selection):
         x,y = selection
-        if self.board_revealed[x][y] == False:
-            self.board_revealed[x][y] = True
+        if (x,y) not in self.revealed_coords:
+            self.revealed_coords.add((x,y))
             selection_value = self.board[x][y]
             if selection_value==0:
                 selection_nbrs = get_neighbours(selection,self.cols,self.rows)
@@ -71,11 +71,13 @@ class game:
         if self.gameState == "gameover":
             for mine in self.mine_coords:
                 x,y = mine
-                self.board_revealed[x][y] = True
-        board_disp = np.array(self.board).astype("str")
-        board_revealed_temp = np.array(self.board_revealed)
+                self.revealed_coords.add((x,y))
+        board_disp = [[" " for i in range(0,self.rows)] for j in range(0,self.cols)]
+        for coord in self.revealed_coords:
+            x,y = coord
+            board_disp[x][y] = self.board[x][y]
 
-        board_disp[board_revealed_temp==False] = " "
+        board_disp = np.array(board_disp).astype("str")
         board_disp[board_disp=="9"] = "M"
         board_disp[board_disp=="10"] = "F"
         board_disp[board_disp=="11"] = "B"
@@ -98,12 +100,53 @@ class game:
             self.show_board()
         while self.gameState == "playing":
             self.play_turn()
-            if self.board_revealed == [[True for i in range(0,self.rows)] for j in range(0,self.cols)]:
+            if len(self.revealed_coords)==self.cols*self.rows:
                 self.gameState = "winner"
                 print("Winner winner chicken dinner")
 
+class Player:
+    def __init__(self,game):
+        self.game = game
+        self.active_cells = set([])
+        self.flagged_coords = set([])
+    def make_first_move(self):
+        self.game.make_first_move()
+        self.refresh_active_cells()
+    def refresh_active_cells(self):
+        for coord in self.game.revealed_coords:
+            nbrs = get_neighbours(coord,self.game.cols,self.game.rows)
+            if len(self.game.revealed_coords.intersection(nbrs))!=len(nbrs):
+                self.active_cells.add(coord)
+            elif coord in self.active_cells:
+                self.active_cells.remove(coord)
+    def find_mines(self,coord,nbrs):
+        x,y = coord
+        unrevealed_nbrs = self.game.revealed_coords.intersection(nbrs)
+        if len(unrevealed_nbrs) == self.game.board[x][y]:
+            for cell in unrevealed_nbrs:
+                self.game.flag_cell(cell)
+                self.flagged_coords.add(cell)
+    def find_safe(self,coord,nbrs):
+        x,y = coord 
+        flagged_nbrs = self.flagged_coords.intersection(nbrs)
+        if len(flagged_nbrs) == self.game.board[x][y]:
+            for cell in flagged_nbrs:
+                self.game.select_cell(cell)
+    def play_turn(self):
+        for coord in self.active_cells:
+            nbrs = get_neighbours(coord,self.game.cols,self.game.rows)
+            self.find_mines(coord,nbrs)
+            self.find_safe(coord,nbrs)
+        self.refresh_active_cells()
+    def play_game(self):
+        self.make_first_move()
+        self.game.show_board()
+        while self.game.gameState == "playing":
+            self.play_turn()
+
+
+
 #initialise game
-currentGame = game(5,5,5)
-currentGame.show_board()
-#first move (generate board once first move is made)
-currentGame.play_game()
+currentGame = Game(5,5,5)
+currentPlayer = Player(currentGame)
+currentPlayer.play_game()
